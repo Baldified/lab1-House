0cd5da07cf31e233b19a31cee33ed1fffc612faf
Baldified <88463745+Baldified@users.noreply.github.com>
2025-08-26 20:57:45 -0400

    Update Picture.java

--- Picture.java
+++ Picture.java
@@ -6,7 +6,7 @@
  *
  * This class was written as an early example for teaching Java with BlueJ.
  * 
- * @author  Michael Kšlling and David J. Barnes
+ * @author  Michael KÅ¡lling and David J. Barnes
  * @version 7.0
  */
 public class Picture
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Circle sun2;
     private boolean drawn;
 
     /**
@@ -26,6 +27,7 @@ public class Picture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
+        sun2 = new Circle();
         drawn = false;
     }
 
@@ -56,6 +58,12 @@ public class Picture
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
+
+            sun2.changeColor("blue");
+            sun2.moveHorizontal(150);
+            sun2.moveVertical(-650);
+            sun2.changeSize(80);
+            sun2.makeVisible();
             drawn = true;
         }
     }
